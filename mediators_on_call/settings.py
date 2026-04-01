964fdb82572ee1ab9d585c28e18a71266b1a56a2
JVW-Hue <janovanwyk553@gmail.com>
2026-04-01 23:15:51 +0200

    Use relative path for database

--- mediators_on_call/settings.py
+++ mediators_on_call/settings.py
@@ -127,7 +127,7 @@ import os
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/opt/render/project/src/db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
