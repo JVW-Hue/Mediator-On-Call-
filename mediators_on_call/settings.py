a4e07353a0704206e06e12dfa749f64e96936098
JVW-Hue <janovanwyk553@gmail.com>
2026-04-09 18:36:30 +0200

    Revert to SQLite database with migrations on startup

--- mediators_on_call/settings.py
+++ mediators_on_call/settings.py
@@ -122,24 +122,12 @@ WSGI_APPLICATION = 'mediators_on_call.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-import os
-import dj_database_url
-
-# Use PostgreSQL if DATABASE_URL is provided (Render managed database)
-# Otherwise fall back to SQLite for local development
-DATABASE_URL = os.environ.get('DATABASE_URL')
-
-if DATABASE_URL:
-    DATABASES = {
-        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 
 # Password validation
