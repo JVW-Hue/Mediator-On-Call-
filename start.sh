a869bdf88cfefd155889b31622348287039a877b
JVW-Hue <janovanwyk553@gmail.com>
2026-04-09 16:51:50 +0200

    Make Frank Stanley account a superuser with full admin access to manage all disputes

--- start.sh
+++ start.sh
@@ -11,7 +11,7 @@ from django.contrib.auth import get_user_model
 from disputes.models import Mediator
 User = get_user_model()
 for udata in [
-    {'username':'frankstanley','email':'frank@probonomediation.co.za','first_name':'Frank','last_name':'Stanley','password':'FrankStanley2026!','is_staff':True},
+     {'username':'frankstanley','email':'frank@probonomediation.co.za','first_name':'Frank','last_name':'Stanley','password':'FrankStanley2026!','is_staff':True, 'is_superuser': True},
     {'username':'JVW','email':'jvw@probonomediation.co.za','password':'JVW123','is_staff':True},
     {'username':'admin','email':'admin@probonomediation.co.za','first_name':'Admin','password':'Admin2026!','is_staff':True},
 ]:
